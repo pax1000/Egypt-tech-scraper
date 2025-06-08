f9d14482af59cecc9bd93d25cf3f1bd692ab5678
mohanad <mohanadhessen@gmail.com>
2025-06-08 06:10:05 +0300

    modification on root to fix some bugs

--- root.py
+++ root.py
@@ -2,7 +2,6 @@ from sigma import sigma_scraper
 from elnekhely import elnekhelyt_scraper
 from elbadrgroupeg import elbadrgroupeg_scraper
 import json
-import re
 
 #* Get data from different sites and merge them into a single JSON file
 def get_data(product_name = None):
@@ -14,27 +13,34 @@ def get_data(product_name = None):
         product_name = user_input
 
     try:
+        # Initialize empty data.json
+        with open("data.json", "w", encoding='utf-8') as f:
+            json.dump([], f)
+            
         # Scrape data from sigma site and load it from data.json
         sigma_scraper(product_name)
-        with open("data.json", "r") as f:
+        with open("data.json", "r", encoding='utf-8') as f:
             sigma_data = json.load(f)
         
         # Scrape data from elnekhely site and load updated data from data.json
         elnekhelyt_scraper(product_name)
-        with open('data.json','r') as f:
+        with open('data.json','r', encoding='utf-8') as f:
             elnekhelyt_data = json.load(f)
         
         # Scrape data from elbadrgroupeg site and load updated data from data.json
         elbadrgroupeg_scraper(product_name)
-        with open('data.json','r') as f:
+        with open('data.json','r', encoding='utf-8') as f:
             elbadrgroupeg_data = json.load(f)
     
         # Merge all the scraped data lists
         merged_data = sigma_data + elnekhelyt_data + elbadrgroupeg_data
         
         # Write the merged data back to data.json with pretty formatting
-        with open("data.json", "w") as f:
-            json.dump(merged_data, f, indent=2)
+        with open("data.json", "w", encoding='utf-8') as f:
+            json.dump(merged_data, f, indent=2, ensure_ascii=False)
+            
+        
+        
     except Exception as e:
         index = 3
         print(f'error : {e} number of retries left {index}')
@@ -43,14 +49,5 @@ def get_data(product_name = None):
             index -=1
 
 
-with open("data.json", "r") as f:
-    products_data = json.load(f)
-
-
-
-    
-    
-# Call the functions 
-get_data()
-
-
+if __name__ == "__main__":
+    get_data()
